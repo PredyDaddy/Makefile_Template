83c4aee6dd5e9bc385fdb7f8ea08750ff2176a12
predy <1642942273@qq.com>
2023-02-26 22:54:39 +0800

    git@github.com:PredyDaddy/Makefile_Template.git

---
+++ src/01kernel.cu
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+__global__ void add(int a, int b, int *c)
+{
+    *c = a + b;
+}
+
+void func()
+{
+    int a = 2, b = 3, c;
+    int *d_c;
+
+    cudaMalloc((void **)&d_c, sizeof(int));
+
+    cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+    std::cout << a << " + " << b << " = " << c << std::endl;
+
+    cudaFree(d_c);
+}
